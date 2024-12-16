bec42c1626b1c1f93433f0f23ea716df1d35e0ba
ryanGT <ryanwkrauss@gmail.com>
2024-12-15 19:32:27 -0500

    python install v1

---
+++ python_installs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt install python3-numpy
+sudo apt install python3-scipy
+sudo apt install python3-matplotlib
+sudo apt install python3-ipython
+sudo apt install python3-dev python3-pip python3-venv nodejs npm
+sudo apt install python3-ipython
+sudo apt install python3-pandas
+sudo apt install python3 python3-pip -y
+sudo apt install python3-dev python3-pip python3-venv nodejs npm
+sudo apt install ipython3
